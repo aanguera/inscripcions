d458360fa5a0bf6bc8c3e8f57b908d1c82ddbbf5
aanguera <aanguera.65@gmail.com>
2016-09-24 13:31:24 +0200

    Actualització que permet guardar les dades a la base de dades en el següent pas

--- pas1.php
+++ pas1.php
@@ -1,4 +1,5 @@
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
    <head>
       <title>Inscripcions Hivernal del Bages</title>
    </head>
@@ -10,10 +11,13 @@
 			<legend>Dades personals</legend>
 			<p><label>Non: <input type="text" name="nom" size="30" maxlength="50"> </label></p>
 			<p><label>Cognoms: <input type="text" name="cognoms" size="60" maxlength="80"> </label></p>
+			<p><label>Email: <input type="text" name="email" size="60" maxlength="80"> </label></p>
+			<p><label>Mòbil: <input type="text" name="mobil1" size="9" maxlength="13"> </label></p>
+			<p><label>Telf. cas d'emergència: <input type="text" name="telf2" size="9" maxlength="13"> </label></p>
 			<p><label for="type">Federació</label>
-			<input type="radio" name="federat" value="S" /> Federat
-			<input type="radio" name="gender" value="N" /> NO Federat
-			</p>		
+			<input type="radio" name="federacio" value="F" /> Federat
+			<input type="radio" name="federacio" value="N" /> No federat
+			</p>
 		</fieldset>
 		<p><input type="submit" name="submit" value="Següent"</p>
 		
